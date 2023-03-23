e12d38b2a64b15e7b88427b2e27c23b588fdaa35
tejastamkar <tejas.tamkar@gmail.com>
2023-03-23 15:10:07 +0530

    added analysis api added

---
+++ analysis.py
@@ -0,0 +1,72 @@
+import pytz
+from datetime import timedelta
+from datetime import datetime
+
+
+def getMonthDetails(data):
+    sortedData = getDataSort(data)
+    # df = sortedData
+    week1_df = []
+    week2_df = []
+    week3_df = []
+    week4_df = []
+
+    today = datetime.today()
+    week1_start = today - timedelta(weeks=4)
+    week1_end = today - timedelta(weeks=3) + timedelta(days=1)
+    week2_start = today - timedelta(weeks=3)
+    week2_end = today - timedelta(weeks=2) + timedelta(days=1)
+    week3_start = today - timedelta(weeks=2)
+    week3_end = today - timedelta(weeks=1) + timedelta(days=1)
+    week4_start = today - timedelta(weeks=1)
+    week4_end = today
+
+    utc = pytz.UTC
+
+    week1_start = utc.localize(week1_start)
+    week1_end = utc.localize(week1_end)
+    week2_start = utc.localize(week2_start)
+    week2_end = utc.localize(week2_end)
+    week3_start = utc.localize(week3_start)
+    week3_end = utc.localize(week3_end)
+    week4_start = utc.localize(week4_start)
+    week4_end = utc.localize(week4_end)
+
+    for df in sortedData:
+        if df["Time"] >= week1_start and df['Time'] <= week1_end:
+            week1_df.append(df)
+        elif df["Time"] >= week2_start and df['Time'] <= week2_end:
+            week2_df.append(df)
+        elif df["Time"] >= week3_start and df['Time'] <= week3_end:
+            week3_df.append(df)
+        elif df["Time"] >= week4_start and df['Time'] <= week4_end:
+            week4_df.append(df)
+
+    w1 = getMap(week1_df, "w1")
+    w2 = getMap(week2_df, "w2")
+    w3 = getMap(week3_df, "w3")
+    w4 = getMap(week4_df, "w4")
+
+    return [w1, w2, w3, w4]
+
+
+def getMap(dataSet, nameOFaxis: str):
+    y1 = 0
+    y2 = 0
+    x = []
+    for data in dataSet:
+        if (data["TypeOfDonation"] != "Good"):
+            y1 += 1
+        else:
+            y2 += 1
+    x.append(nameOFaxis)
+    x.append(y1)
+    x.append(y2)
+    return x
+# getMonthDetails()
+
+
+def getDataSort(donationData):
+    sorted_arr = sorted(
+        donationData, key=lambda x: x["Time"], reverse=True)
+    return sorted_arr
--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request
+from analysis import getMonthDetails
 from rank import getRank, MyRank
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from datetime import timedelta
+from datetime import datetime
 
 cred = credentials.Certificate("./serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
@@ -14,10 +17,15 @@ app.config["DEBUG"] = True
 db = firestore.client()
 
 
+@app.route("/")
+def home():
+    return f"/rank , /myrank , /getanalysis"
+
+
 @app.route("/rank")
 def getrank():
     userData = []
-    docsnap = db.collection('Users').get()
+    docsnap = db.collection('Users').where(u'Points', u'>=', 10).get()
     for doc in docsnap:
         userData.append(doc.to_dict())
     myresults = getRank(userData)
@@ -33,7 +41,21 @@ def getMyRank():
         userData.append(doc.to_dict())
     sortedList = getRank(userData)
     myresults = MyRank(sortedList, userid)
-    return f"{myresults}"
+    return f"{myresults +1}"
+
+
+@app.route("/getanalysis")
+def getAnalysis():
+    # userid = request.form['uid']
+    date = datetime.today() - timedelta(weeks=4)
+    donationData = []
+    docsnap = db.collection('Donation').where(u'Time', u'>=', date).get()
+    for doc in docsnap:
+        donationData.append(doc.to_dict())
+    # print(donationData)
+    sortedList = getMonthDetails(donationData)
+    # myresults = MyRank(sortedList, userid)
+    return sortedList
 
 
 if __name__ == "__main__":
